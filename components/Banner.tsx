d8684c0e4455ea4115c4cbfd7e6e63953b6da3c0
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2023-01-01 17:59:21 +0100

    Done Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,17 @@ import React from "react";
 type Props = {};
 
 const Banner = (props: Props) => {
-	return <div>Banner</div>;
+	return (
+		<div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10'>
+			<div>
+				<h1 className='text-5xl'>Personal Blog</h1>
+				<h2 className='mt-5 md:mt-2'>Welcome to this part of my website!</h2>
+			</div>
+			<p className='mt-5 md:mt-2 text-gray-400 max-w-lg'>
+				Details on my projects | Progress on my programming path | Much more...
+			</p>
+		</div>
+	);
 };
 
 export default Banner;
